d8120b1c129ac0c55e5e3ea82e4870b66a621c11
prakashsanas02 <156497321+prakashsanas02@users.noreply.github.com>
2024-02-03 13:40:41 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,28 @@
+// Q.1
+let a = "Prakash";
+let b = 8;
+console.log(a + b);
+
+// Q.2
+console.log(typeof (a + b));
+
+// Q.3
+const a1 = {
+  name: "Prakash",
+  section: 1,
+  isPrincipal: false,
+};
+
+// Q.4
+a1["friend"] = "Jaydeep";
+a1["name"] = "Vibhas";
+console.log(a1);
+
+// Q.5
+const dict = {
+  appreciate: "recognize the full worth of.",
+  ataraxia: "a state of freedom from emotional disturbance and anxiety",
+  yakka: "work, especially hard work.",
+};
+
+console.log(dict["ataraxia"]);
